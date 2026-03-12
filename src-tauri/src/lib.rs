664811849c47c1d19295cf82ebb78f0734fd7b86
jjff11 <93717340+jjff11@users.noreply.github.com>
2026-03-12 14:40:05 +0800

    Update lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -22,8 +22,8 @@ fn get_settings(handle: &tauri::AppHandle) -> AppSettings {
     let store = handle.store("settings").unwrap();
     let result = store.get("app_settings");
     match result {
-        Ok(Some(value)) => serde_json::from_value(value).unwrap_or(AppSettings::default()),
-        _ => AppSettings::default(),
+    Some(value) => serde_json::from_value(value).unwrap_or(AppSettings::default()),
+    None => AppSettings::default(),
     }
 }
 
